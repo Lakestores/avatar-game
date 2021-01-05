a3bd1b6b689c420f01587fd01e11fc9578d3b855
Luna Belfort <sovietlunox@gmail.com>
2021-01-05 18:29:16 -0300

    Feature update #1
    -Added Font method
    -Added Navigation
    -Added App Loading
    -Added Screens:
        -Login
        -Register
        -Profile

--- avatarGame/App.js
+++ avatarGame/App.js
@@ -1,33 +1,66 @@
 //import system
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-//import assets
-import logo from './imgs/logo.gif'; 
+import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import AppLoading from 'expo-app-loading';
 
-export default function App() {
+const Stack = createStackNavigator();
+//import assets
+import logo from './assets/imgs/logo.gif'; 
+
+//Font thing
+import { useFonts } from 'expo-font';
+
+import { setCustomText } from 'react-native-global-props';
+const customTextProps = { 
+  style: { 
+    //fontFamily: 'Avatar',
+  }
+}
+setCustomText(customTextProps);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Homescreen
+function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-        <View style={styles.header}>
+      <View style={styles.header}>
         <Text style={styles.title}>
-        {'\n'}Avatar Game
+          {'\n'}Avatar Games
         </Text>
         <Image source={logo} style={styles.logo} />
       </View>
-      
+
       <View style={styles.menu}>
         <View style={styles.submenu}>
           <Text style={styles.menutext} >
             Use the Login or Register button below to start{'\n'}{'\n'}
           </Text>
-          
+        
           <TouchableOpacity
-            onPress={() => alert('Login Failed')}
+            onPress={() => navigation.navigate('LoginScreen')}
             style={styles.button}>
             <Text style={styles.buttonText}>Login</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            onPress={() => alert('Not available yet')}
+            onPress={() => navigation.navigate('RegisterScreen')}
             style={styles.button}>
             <Text style={styles.buttonText}>Register</Text>
           </TouchableOpacity>
@@ -36,12 +69,117 @@ export default function App() {
           <Text style={styles.license}> Created by Luna and Soul</Text>
         </View>
       </View>
-      
       <StatusBar style="auto" />
-
     </View>
   );
 }
+
+//Profile Screen
+function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Profile Screen</Text>
+    </View>
+  );
+}
+
+function LoginScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Login Screen</Text>
+    </View>
+  );
+}
+function RegisterScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Register Screen</Text>
+    </View>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+//App and menu
+export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    //'Avatar': require('./assets/fonts/avatar.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } 
+
+
+  else {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: 'Home' }}
+          />
+
+          <Stack.Screen
+            name="ProfileScreen"
+            component={ProfileScreen}
+            options={{ title: 'Profile' }}
+          />
+
+          <Stack.Screen
+            name="LoginScreen"
+            component={LoginScreen}
+            options={{ title: 'Login' }}
+          />
+          <Stack.Screen
+            name="RegisterScreen"
+            component={RegisterScreen}
+            options={{ title: 'Register' }}
+          />
+        </Stack.Navigator>
+        
+      </NavigationContainer>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -67,7 +205,7 @@ const styles = StyleSheet.create({
   },
   bottom: {
     position: "absolute",
-    top: 481,
+    top: 400,
     width:380,
     backgroundColor: "grey"
   },
@@ -96,8 +234,9 @@ const styles = StyleSheet.create({
     color: '#fff',
   },
   title: {
-    color: '#ff0000', 
+    color: '#FFFFFF', 
     fontSize: 40,
+ 
   },
   license: {
     
