77840af3dae16dd10e0a937e995fcd1b438ff235
Luna Belfort <sovietlunox@gmail.com>
2021-01-07 20:16:18 -0300

    Feature Update #5
    -Added Menu Bar

--- avatarGame/Profile.js
+++ avatarGame/Profile.js
@@ -5,15 +5,17 @@ import 'react-native-gesture-handler';
 
 import GlobalState from './contexts/GlobalState';
 
-//username,password,
+//Import files
+import MenuBar from './menu';
+
 function ProfileScreen({navigation}) {
     const [state, setState] = useContext(GlobalState);
 
     return (
         <View style={styles.mainbox}>
             <Text style={styles.title}>{state.username}</Text>
-        </View>
-
+            <MenuBar/>
+        </View>  
     );
 }
 
@@ -22,6 +24,8 @@ const styles = StyleSheet.create({
     mainbox: {
         marginTop: 10,
         marginHorizontal: 10,
+        width: 360,
+        height: 800,
     },
     title: {
         fontSize: 40,
---
+++ avatarGame/menu.js
@@ -0,0 +1,76 @@
+//system imports
+import React, { useContext, useEffect } from 'react';
+import { TextInput, Image, StyleSheet, Text, TouchableOpacity, View, Button, Alert } from 'react-native';
+import 'react-native-gesture-handler';
+
+import GlobalState from './contexts/GlobalState';
+
+
+function MenuBar({}) {
+    const [state, setState] = useContext(GlobalState);
+    const menustyle = StyleSheet.create({
+        button: {
+            height: 70,
+            width: 75,
+            backgroundColor: 'blue',
+            borderRadius: 5,
+            borderWidth: 2,
+            marginHorizontal: 32,
+            marginVertical: 10,
+            
+        },
+        boxxy: {
+            alignContent: 'center',
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            position: "absolute",
+            top: 623,
+            width:410,
+            right: -25,
+            height: 100,
+            backgroundColor: 'green',
+            borderWidth: 3,
+        },
+        textys: {
+            textAlign: 'center',
+            marginTop: 20,
+            color: 'white',
+        },
+    });
+
+    return (
+        <View style={menustyle.boxxy}>
+            
+            <TouchableOpacity style={menustyle.button}>
+                <Text style={menustyle.textys}>Map</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={menustyle.button}>
+                <Text style={menustyle.textys}>No idea</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={menustyle.button}>
+                <Text style={menustyle.textys}>Settings</Text>
+            </TouchableOpacity>
+        </View>
+
+    );
+}
+
+
+const menustyle = StyleSheet.create({
+    box: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: 200,
+        marginVertical: 10,
+        backgroundColor: 'green',
+        height: 200,
+    },
+    button: {
+    },
+});
+
+export default MenuBar;
